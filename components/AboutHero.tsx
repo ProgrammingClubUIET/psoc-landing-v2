f9f6ca6dffef60c581bd7096a0481ce860d86648
rupansh <rupanshsekar@hotmail.com>
2022-08-11 21:07:50 +0530

    AboutHero: fix derp

--- components/AboutHero.tsx
+++ components/AboutHero.tsx
@@ -8,7 +8,7 @@ const AboutHero = () => (
     <VStack pt="10vh" bgGradient={HERO_GRAD} width="full" height="100vh" boxShadow="0 2xl 2xl -2xl" spacing="8rem">
         <Image alt="PSoC Logo" boxSize="xs" src="/img/psoc-logo-2.png"></Image>
         <SimpleGrid width="full" columns={[2, null, 4]}>
-            {STATS.map((v, i) => StatBox({ color: "white", ...v, key: i}))}
+            {STATS.map((v, i) => StatBox({ color: "white", ...v }))}
         </SimpleGrid>
     </VStack>
 );
