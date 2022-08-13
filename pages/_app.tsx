ff4341c338ac085fc3af50a67cbc9a898c44d78c
rupansh <rupanshsekar@hotmail.com>
2022-08-13 22:59:24 +0530

    update title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import { ChakraProvider, Flex } from '@chakra-ui/react';
 import theme from "../theme";
 import NavBar from '../components/NavBar';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return <ChakraProvider theme={theme}>
+        <Head>
+          <title>PSoC 2022</title>
+        </Head>
         <NavBar key={router.asPath}></NavBar>
         <Component  {...pageProps}/>
     </ChakraProvider>
